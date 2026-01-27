44941d9c46ac0d2cfc84fd69049755870e229507
lifeofthecoders <your-email@domain.com>
2026-01-27 19:27:47 +0530

    Save New Changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -96,16 +96,8 @@ export default function Contact() {
                   and detailed QA project experience.
                 </p>
 
-                {/* <a
-                  href="/resume/Sumit_Panchal_QA_Resume.pdf"
-                  className="btn-primary"
-                  download="Sumit_Panchal_QA_Resume.pdf"
-                >
-                  <b>Download Resume (PDF)</b>
-                </a> */}
-
                 <a
-                  href={`${process.env.PUBLIC_URL}/assets/resume/Sumit_Panchal_QA_Resume.pdf`}
+                  href="/resume/Sumit_Panchal_QA_Resume.pdf"
                   download
                   target="_blank"
                   rel="noopener noreferrer"
